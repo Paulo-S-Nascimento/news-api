ddeb0c8c57693e5084d4fd0d5242e322aa9a4692
Paulo Nascimento <ppnascimentoo@hotmail.com>
2024-10-05 15:14:00 -0300

    news-api

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,37 @@
-import express from 'express'
-import { routes } from './routes'
+import express from 'express';
+import { routes } from './routes';
+import * as dotenv from 'dotenv';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsdoc from 'swagger-jsdoc';
 
-const app = express()
-app.use(express.json())
+dotenv.config();
 
-app.use('/api/v1', routes)
+const app = express();
+app.use(express.json());
 
-export { app }
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0', 
+    info: {
+      title: 'News API', 
+      version: '1.0.0', 
+      description: 'API para gerenciamento de notícias',
+    },
+    servers: [
+      {
+        url: `http://localhost:${process.env.PORT || 5432}/api/v1`, 
+      },
+    ],
+  },
+  apis: ['./routes/*.ts'], 
+};
 
 
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
 
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+app.use('/api/v1', routes);
+
+export { app };
